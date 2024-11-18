1fe6793db9b1cd5feefcc7ac6e7c5be5a616c6b9
Dalton Muck <dalton.muck22@icloud.com>
2024-11-18 15:47:41 -0500

    View Tables

---
+++ ViewTables.sql
@@ -0,0 +1,25 @@
+SELECT * FROM Toppings;
+
+SELECT * FROM Pizza;
+
+SELECT * FROM ToppingsOnPizza;
+
+SELECT * FROM Discount;
+
+SELECT * FROM DiscountOnPizza;
+
+SELECT * FROM DiscountOnOrder;
+
+SELECT * FROM BasePrice;
+
+SELECT * FROM Orders;
+
+SELECT * FROM Customer;
+
+SELECT * FROM Delivery;
+
+SELECT * FROM Pickup;
+
+SELECT * FROM DineIn;
+
+SELECT * FROM Seats;
